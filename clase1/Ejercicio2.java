8db60afa7502a33ed62c0beb02464c14dff45499
Maaurimiranda <mirandamauri242@gmail.com>
2026-08-14 22:29:12 -0300

    (clase 1): Ejercicio 2

---
+++ clase1/Ejercicio2.java
@@ -0,0 +1,11 @@
+package clase1;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Integer maximo: " + Integer.MAX_VALUE);
+        System.out.println("Integer maximo más uno: " + (Integer.MAX_VALUE + 1));
+
+        System.out.println("Long maximo: " + Long.MAX_VALUE);
+        System.out.println("Long maximo más uno: " + (Long.MAX_VALUE + 1));
+    }
+}
